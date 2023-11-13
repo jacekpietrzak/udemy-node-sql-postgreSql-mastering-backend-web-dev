26ddaed5d744dfc2d96e1080ebf741b3517def14
Jacek Pietrzak <jacekpietrzak@me.com>
2023-11-13 15:40:28 +0100

    creaturesdb

--- psql/creatures.sql
+++ psql/creatures.sql
@@ -17,6 +17,17 @@ CREATE TABLE hobbits(
     personality character varying(50)
 );
 
+-- Relation tables
+CREATE TABLE allies(
+    wizard character varying(50),
+    elf character varying(50)
+);
+
+CREATE TABLE guardians(
+    elf character varying(50),
+    hobbit character varying(50)
+);
+
 -- INSERT INTO whichTable(schema) = insert data
 -- VALUES = insert values
 INSERT INTO wizards(name,power)
@@ -30,3 +41,22 @@ VALUES
 ('Legolas',10),
 ('Arwen',9),
 ('Elrond',5);
+
+INSERT INTO hobbits(name, personality)
+VALUES
+('Frodo','careful'),
+('Sam','brave'),
+('Bilbo','reedy');
+
+INSERT INTO allies(wizard, elf)
+VALUES
+('Gandalf', 'Legolas'),
+('Gandalf', 'Arwen'),
+('Saruman', 'Elrond'),
+('Saruman', 'Legolas');
+
+INSERT INTO guardians(elf, hobbit)
+VALUES
+('Legolas', 'Frodo'),
+('Arwen','Sam'),
+('Elrond','Bilbo');
